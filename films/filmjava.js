a0d5df72c0221f8d1e6f2bba48dffbc6e571bb56
Jaycee Morrill <jayceelm.ut@gmail.com>
2021-03-25 19:41:15 -0600

    more star backgrounds!

--- films/filmjava.js
+++ films/filmjava.js
@@ -27,3 +27,27 @@ for (let i = 0; i < films.length; i++) {
 
 }
 
+function addStarField(element, numStars) {
+    element.style.setProperty('background-color', 'black')
+    for (let i = 0; i < numStars; i++) {
+        let star = document.createElement('div')
+        star.style.setProperty('width', '1px')
+        star.style.setProperty('height', '1px')
+        star.style.setProperty('background-color', 'aliceblue')
+        let xy = getRandomPosition()
+        star.style.left = `${xy[0]}px`
+        star.style.top = `${xy[1]}px`
+        star.style.setProperty('position', 'absolute')
+        element.appendChild(star)
+    }
+}
+
+function getRandomPosition() {
+    let y = document.body.scrollHeight
+    let x = document.body.scrollWidth
+    let randomY = Math.floor(Math.random() * y)
+    let randomX = Math.floor(Math.random() * x)
+    return [randomX, randomY]
+}
+
+addStarField(document.querySelector('body'), 900)
