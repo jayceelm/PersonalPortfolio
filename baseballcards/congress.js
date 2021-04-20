ba8e9819938989c2dbb14ef0a075ce2d39ea506b
Jaycee Morrill <jayceelm.ut@gmail.com>
2021-04-20 16:41:02 -0600

    congress js and css (images+)

--- baseballcards/congress.js
+++ baseballcards/congress.js
@@ -1,7 +1,41 @@
 import { senators } from "../data/senators.js";
 import { representatives } from "../data/representatives.js";
 
-const demButton = document.querySelector("#democrats");
+const congressGrid = document.querySelector(".congressGrid");
+const stateButton = document.querySelector("#state");
+
+function populateCongressGrid(simpleList) {
+  simpleList.forEach((person) => {
+    let personDiv = document.createElement("div");
+    //personDiv.className = 'figureDiv'//
+    let personFig = document.createElement("figure");
+    let figImg = document.createElement("img");
+    let figCaption = document.createElement("figcaption");
+
+    figImg.src = person.imgURL;
+    figCaption.textContent = person.name;
+
+    personFig.appendChild(figImg);
+    personFig.appendChild(figCaption);
+    personDiv.appendChild(personFig);
+    congressGrid.appendChild(personDiv);
+  });
+}
+
+function getSimplifiedPeople(peopleList) {
+  return peopleList.map((person) => {
+    let middleName = person.middle_name ? ` ${person.middle_name}` : ``;
+    return {
+      name: `${person.first_name}${middleName} ${person.last_name}`,
+      state: person.state,
+      imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`,
+    };
+  });
+}
+
+populateCongressGrid(getSimplifiedPeople(representatives));
+
+/* const demButton = document.querySelector("#democrats");
 
 demButton.addEventListener("click", () => {
   showDemocrats();
@@ -20,4 +54,4 @@ function showDemocrats() {
 }
 
 //const repButton = document.querySelector('.republicans')//
-//const indButton = document.querySelector('.independents')//
+//const indButton = document.querySelector('.independents')// */
