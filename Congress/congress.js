6a634f304894cf3a110c3bc641ba8a4ac6bd6ef7
Jaycee Morrill <jayceelm.ut@gmail.com>
2021-04-26 22:51:32 -0600

    added buttons, but not sorting?

--- Congress/congress.js
+++ Congress/congress.js
@@ -1,27 +1,33 @@
 import { senators } from "../data/senators.js";
 import { representatives } from "../data/representatives.js";
-import { removeChildren } from "../utils/index.js";
 
 const congressGrid = document.querySelector(".congressGrid");
-const demButton = document.querySelector("#democrats");
+const democratButton = document.querySelector("#democrats");
 const repButton = document.querySelector("#republicans");
-const indButton = document.querySelector("#independents");
+const Button = document.querySelector("#republicans");
 
-republicansButton.addEventListener("click", () => {
+democratButton.addEventListener("click", () => {
+  populateCongressDiv(filterCongressPeople(representatives, "D"));
+});
+
+repButton.addEventListener("click", () => {
   populateCongressDiv(filterCongressPeople(representatives, "R"));
 });
 
-function populateCongressDiv(simplifiedList) {
-  removeChildren(congressGrid);
-  simplifiedList.forEach((person) => {
+stateButton.addEventListener('click', () => stateSort());
+
+
+function populateCongressGrid(simpleList) {
+  simpleList.forEach((person) => {
     let personDiv = document.createElement("div");
-    personDiv.className = "figureDiv";
+    //personDiv.className = 'figureDiv'//
     let personFig = document.createElement("figure");
     let figImg = document.createElement("img");
     let figCaption = document.createElement("figcaption");
 
     figImg.src = person.imgURL;
     figCaption.textContent = person.name;
+
     personFig.appendChild(figImg);
     personFig.appendChild(figCaption);
     personDiv.appendChild(personFig);
@@ -33,13 +39,16 @@ function getSimplifiedPeople(peopleList) {
   return peopleList.map((person) => {
     let middleName = person.middle_name ? ` ${person.middle_name}` : ``;
     return {
-      id: person.id,
       name: `${person.first_name}${middleName} ${person.last_name}`,
-      imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`,
       party: person.party,
+      imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`,
     };
   });
 }
+
+
+function stateSort() {
+  populateCongressDiv(getSimplifiedPeople(representatives).sort((a, b) => a.state- b.state))
 
 const filterCongressPeople = (chamber, politicalParty) => {
   return getSimplifiedPeople(chamber).filter(
@@ -47,4 +56,4 @@ const filterCongressPeople = (chamber, politicalParty) => {
   );
 };
 
-populateCongressDiv(getSimplifiedPeople(senators));
+populateCongressGrid(getSimplifiedPeople(representatives));
